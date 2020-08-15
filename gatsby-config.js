2e73067ad80e69f5235fd7203c655774c97216a6
the-duck <markspurgeon96@hotmail.com>
2020-08-16 01:13:28 +0200

    trying the loadable component for map (openlayers)

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
     url: "https://markspurgeon.ch",
   },
   plugins: [
+    `gatsby-plugin-loadable-components-ssr`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-emotion`,
     {
